99bb4a4fc29b8bbfb93b30446b41a5a288085ead
Mateusz Prusinowski <prusinowski.mateusz@gmail.com>
2019-11-03 20:41:14 +0100

    Initial

---
+++ StackNQueue_1/Flapjack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackNQueue_1
+{
+    enum Flapjack
+    {
+        Crispy,
+        Soggy,
+        Browned,
+        Banana,
+    }
+}
---
+++ StackNQueue_1/Lumberjack.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackNQueue_1
+{
+    class Lumberjack
+    {
+        private string name;
+        public string Name { get { return name; } }
+        private Stack<Flapjack> meal;
+        public Lumberjack(string name)
+        {
+            this.name = name;
+            meal = new Stack<Flapjack>;
+        }
+        public int FlapjackCount { get { return meal.Count; } }
+        public void TakeFlapjacks(Flapjack food, int howMany)
+        {
+
+        }
+    }
+}
